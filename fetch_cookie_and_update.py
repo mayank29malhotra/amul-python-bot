765fa9ed0cd403931d43f401d779b9e9ab5fdcef
mayank29malhotra <mayank29malhotra@gmail.com>
2025-06-12 15:39:49 +0530

    updated cookie url

--- fetch_cookie_and_update.py
+++ fetch_cookie_and_update.py
@@ -46,19 +46,7 @@ def fetch_cookie_and_url():
         time.sleep(5)  # Wait for page to load and cookies to be set
         cookies = driver.get_cookies()
         cookie_str = '; '.join([f"{c['name']}={c['value']}" for c in cookies])
-        api_url = (
-            "https://shop.amul.com/api/1/entity/ms.products"
-            "?fields[name]=1&fields[brand]=1&fields[categories]=1&fields[collections]=1"
-            "&fields[alias]=1&fields[sku]=1&fields[price]=1&fields[compare_price]=1"
-            "&fields[original_price]=1&fields[images]=1&fields[metafields]=1&fields[discounts]=1"
-            "&fields[catalog_only]=1&fields[is_catalog]=1&fields[seller]=1&fields[available]=1"
-            "&fields[inventory_quantity]=1&fields[net_quantity]=1&fields[num_reviews]=1"
-            "&fields[avg_rating]=1&fields[inventory_low_stock_quantity]=1"
-            "&fields[inventory_allow_out_of_stock]=1&fields[default_variant]=1&fields[variants]=1"
-            "&fields[lp_seller_ids]=1&facets=true&facetgroup=default_category_facet"
-            "&filters[0][field]=categories&filters[0][value][0]=protein&filters[0][operator]=in"
-            "&filters[0][original]=1&limit=24&total=1&start=0&cdc=1m&substore=66505ff5145c16635e6cc74d"
-        )
+        api_url = ("https://shop.amul.com/api/1/entity/ms.products?fields[name]=1&fields[brand]=1&fields[categories]=1&fields[collections]=1&fields[alias]=1&fields[sku]=1&fields[price]=1&fields[compare_price]=1&fields[original_price]=1&fields[images]=1&fields[metafields]=1&fields[discounts]=1&fields[catalog_only]=1&fields[is_catalog]=1&fields[seller]=1&fields[available]=1&fields[inventory_quantity]=1&fields[net_quantity]=1&fields[num_reviews]=1&fields[avg_rating]=1&fields[inventory_low_stock_quantity]=1&fields[inventory_allow_out_of_stock]=1&fields[default_variant]=1&fields[variants]=1&fields[lp_seller_ids]=1&filters[0][field]=categories&filters[0][value][0]=protein&filters[0][operator]=in&filters[0][original]=1&facets=true&facetgroup=default_category_facet&limit=24&total=1&start=0&cdc=1m&substore=66505ff5145c16635e6cc74d")
         headers = {
             "accept": "application/json, text/plain, */*",
             "accept-encoding": "gzip, deflate, br, zstd",
